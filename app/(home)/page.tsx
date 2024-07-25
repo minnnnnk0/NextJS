7018973bdd6feb1684d751aedea5ffe38a202606
minnnnnk0 <kjjpyckjs@naver.com>
2024-07-25 17:40:47 +0900

    feat: movies 데이터fetching

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,11 +4,21 @@ export const metadata = {
   title: 'Home',
 };
 
-export default function Page() {
+const APP_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(APP_URL);
+  const json = await response.json();
+  return json;
+}
+export default async function HomePage() {
+  const movies = await getMovies();
+
   return (
     <div>
       <Navigation />
-      <h1>Hello</h1>
+      <div>{JSON.stringify(movies)}</div>
     </div>
   );
 }
