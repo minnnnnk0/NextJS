22e9c1b73005b094c9db771abfe22da7651d6d87
minnnnnk0 <kjjpyckjs@naver.com>
2024-07-26 16:48:21 +0900

    style: global css 적용

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,13 +1,14 @@
 'use client';
 
+import styles from '../app/styles/navigation.module.css';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default function Navigation() {
   const path = usePathname();
   return (
-    <nav>
-      <ul>
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
         <li>
           <Link href="/">Home</Link> {path === '/' ? '📍' : ''}
         </li>
