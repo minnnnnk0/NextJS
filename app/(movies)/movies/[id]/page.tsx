0a76227f91de4e1d04874420cb41a1f0977d88ac
minnnnnk0 <kjjpyckjs@naver.com>
2024-07-27 16:19:38 +0900

    feat: Dynamin Metadata 추가

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,19 @@
 import { Suspense } from 'react';
-import MovieInfo from '../../../../components/movie-info';
+import MovieInfo, { getMovie } from '../../../../components/movie-info';
 import MovieVideos from '../../../../components/movie-videos';
 
-export default async function MovieDetail({
-  params: { id },
-}: {
+interface IParams {
   params: { id: string };
-}) {
+}
+
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetailPage({ params: { id } }: IParams) {
   return (
     <div>
       <Suspense fallback={<h2>Loading Movie Info</h2>}>
@@ -19,4 +26,3 @@ export default async function MovieDetail({
     </div>
   );
 }
-
